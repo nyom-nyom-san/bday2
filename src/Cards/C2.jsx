da72c1747fd97093cf093bafb1172c36b5798447
Harith <aqil.harith35@gmail.com>
2025-11-27 10:17:53 +0800

    phase 1

--- src/Cards/C2.jsx
+++ src/Cards/C2.jsx
@@ -4,16 +4,15 @@ export default function C2({ handleNext, handleBefore }) {
     return (
         <div className="w2">
             <div className="left-t">
-                <h2>Happy 18th birthday <br />
-                    <i>You Xuan</i></h2>
+                <h2>Happy 25th birthday <br />
+                    <i>Shahmy Ahmed</i></h2>
             </div>
             <div className="right-t">
-                <p>Congratulations on graduating from being a minor.
+                <p>May Allah S.W.T bless you with good health and happiness on your birthday and the coming year.
                     You're definitely old now. Here are the new skills you are gonna unlock:
                     <br />
                     <br />
-                    <li>Random Grandma back pain</li>
-                    <li>Paying taxes</li>
+                    <li>Random Grandpa back pain</li>
                     <li>More responsibilities you didn't ask for</li>
                 </p>
             </div>
